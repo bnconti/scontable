513188e36e0ff5c59f595c1042861b4c73b3ced0
root <root@localhost.localdomain>
2019-10-11 21:40:42 -0300

    Routing mejorado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LibroDiarioComponent } from './libros/libro-diario/libro-diario.component';
 import { LibroMayorComponent } from './libros/libro-mayor/libro-mayor.component';
+import { Error404Component } from './error404/error404.component';
 
 const routes: Routes = [
-  { path: 'libroDiarioRoute', component: LibroDiarioComponent},
-  { path: 'libroMayorRoute', component: LibroMayorComponent}
+  { path: 'libroDiario', component: LibroDiarioComponent},
+  { path: 'libroMayor', component: LibroMayorComponent},
+  { path: '', redirectTo: '/libroDiario', pathMatch: 'full' },
+  { path: '**', component: Error404Component }
 ]
 
 @NgModule({
