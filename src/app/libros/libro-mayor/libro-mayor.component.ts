694883cd5e891aad0b9c6c3eb23a909f531c4b3a
root <root@localhost.localdomain>
2019-10-16 23:09:07 -0300

    Buscador sin modulo cuenta

--- src/app/libros/libro-mayor/libro-mayor.component.ts
+++ src/app/libros/libro-mayor/libro-mayor.component.ts
@@ -3,8 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AsientoService } from 'src/app/asiento.service';
 import { CuentaMayor } from 'src/app/asiento/asiento.model';
-import { Cuenta } from 'src/app/cuenta/cuenta.model';
-import { CuentaService } from 'src/app/cuenta.service';
 //Se importa el modulo de cuentas para buscar con Nro y nombre
 @Component({
   selector: 'app-libro-mayor',
@@ -15,24 +13,16 @@ export class LibroMayorComponent implements OnInit {
   private movimientos: CuentaMayor[];
   private movimientosSub: Subscription;
 
-  private cuentas: Cuenta[];
-  private cuentasSub: Subscription;
 
   /* ------------------------- */
 
-  constructor(public asientoService: AsientoService, public cuentasService: CuentaService) { }
+  constructor(public asientoService: AsientoService) { }
 
   ngOnInit() {
     this.asientoService.traerMovimientos();
     this.movimientosSub = this.asientoService.traerObservadorMovimientos()
       .subscribe((movimientos: CuentaMayor[]) => {
         this.movimientos = movimientos;
-      });
-
-    this.cuentasService.traerCuentas();
-    this.cuentasSub = this.cuentasService.traerObservadorCuentas()
-      .subscribe((cuentas: Cuenta[]) => {
-        this.cuentas = cuentas;
       });
   }
 
@@ -46,7 +36,4 @@ export class LibroMayorComponent implements OnInit {
     return JSON.stringify(obj);
   }
   
-  public getCuentas(): Cuenta[]{
-    return this.cuentas;
-  }
 }
